9fe142e0c13d479ad5088e4b52aa9627e3978bc2
Vincent <tourneretv5@gmail.com>
2020-11-24 10:33:40 +0100

    test serviceWorker change

--- app.js
+++ app.js
@@ -584,3 +584,39 @@ document.addEventListener("DOMContentLoaded", function (_e) {
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./app.js');
 };
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+});
+
+var cacheName = 'ginkoCache-v1';
+var appShellFiles = [
+    '/',
+    '/index.html',
+    '/app.js',
+    '/style.css',
+    '/icons/icon-32.png',
+    '/icons/icon-64.png',
+    '/icons/icon-128.png',
+    '/icons/icon-168.png',
+    '/icons/icon-180.png',
+    '/icons/icon-192.png',
+    '/icons/icon-256.png',
+    '/icons/icon-512.png',
+    '/icons/favicon.ico',
+    '/icons/maskable_icon.png'
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all: app shell and content');
+            return cache.addAll(appShellFiles);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    console.log('[Service Worker] Fetched resource '+e.request.url);
+});
