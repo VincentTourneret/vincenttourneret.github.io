4dc60bc6f351544ff25731ac995baca2acc2e430
Vincent <tourneretv5@gmail.com>
2020-11-24 11:15:04 +0100

    test serviceWorker change

--- sw.js
+++ sw.js
@@ -5,19 +5,19 @@ self.addEventListener('install', (e) => {
 
     var contentToCache = [
         'index.html',
-        // '/app.js',
-        // '/style.css',
-        // '/favicon.ico',
-        // '/icons/icon-32.png',
-        // '/icons/icon-64.png',
-        // '/icons/icon-96.png',
-        // '/icons/icon-128.png',
-        // '/icons/icon-168.png',
-        // '/icons/icon-180.png',
-        // '/icons/icon-192.png',
-        // '/icons/icon-256.png',
-        // '/icons/icon-512.png',
-        // '/icons/maskable_icon.png'
+        'app.js',
+        'style.css',
+        'favicon.ico',
+        'icons/icon-32.png',
+        'icons/icon-64.png',
+        'icons/icon-96.png',
+        'icons/icon-128.png',
+        'icons/icon-168.png',
+        'icons/icon-180.png',
+        'icons/icon-192.png',
+        'icons/icon-256.png',
+        'icons/icon-512.png',
+        'icons/maskable_icon.png'
     ];
 
     e.waitUntil(
