3774874ccf5581fca978d52144ba7f09e70f1b96
Vincent <tourneretv5@gmail.com>
2020-11-24 11:02:19 +0100

    test serviceWorker change

--- sw.js
+++ sw.js
@@ -1,26 +1,23 @@
-
-var cacheName = 'ginko-v1';
-
-var contentToCache = [
-    '/',
-    '/index.html',
-    '/app.js',
-    '/style.css',
-    '/favicon.ico',
-    '/icons/icon-32.png',
-    '/icons/icon-64.png',
-    '/icons/icon-96.png',
-    '/icons/icon-128.png',
-    '/icons/icon-168.png',
-    '/icons/icon-192.png',
-    '/icons/icon-256.png',
-    '/icons/icon-512.png',
-    '/icons/maskable_icon.png',
-];
-
-
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
+    var cacheName = 'ginko-v1';
+
+    var contentToCache = [
+        '/',
+        '/index.html',
+        '/app.js',
+        '/style.css',
+        '/favicon.ico',
+        '/icons/icon-32.png',
+        '/icons/icon-64.png',
+        '/icons/icon-96.png',
+        '/icons/icon-128.png',
+        '/icons/icon-168.png',
+        '/icons/icon-192.png',
+        '/icons/icon-256.png',
+        '/icons/icon-512.png',
+        '/icons/maskable_icon.png',
+    ];
     e.waitUntil(
         caches.open(cacheName).then((cache) => {
             console.log('[Service Worker] Caching all: app shell and content');
@@ -28,6 +25,10 @@ self.addEventListener('install', (e) => {
         })
     );
 });
+
+
+
+
 
 self.addEventListener('fetch', (e) => {
     e.respondWith(
