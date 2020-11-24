a8a6fca8210ffcea556577153c63013879b2efc2
Vincent <tourneretv5@gmail.com>
2020-11-24 10:59:18 +0100

    test serviceWorker change

--- sw.js
+++ sw.js
@@ -1,25 +1,27 @@
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
+
+    var cacheName = 'ginko-v1';
+
+    var contentToCache = [
+        '/',
+        '/index.html',
+        '/app.js',
+        '/style.css',
+        '/favicon.ico',
+        '/icons/icon-32.png',
+        '/icons/icon-64.png',
+        '/icons/icon-96.png',
+        '/icons/icon-128.png',
+        '/icons/icon-168.png',
+        '/icons/icon-192.png',
+        '/icons/icon-256.png',
+        '/icons/icon-512.png',
+        '/icons/maskable_icon.png',
+    ];
 });
 
-var cacheName = 'ginko-v1';
 
-var contentToCache = [
-    '/',
-    '/index.html',
-    '/app.js',
-    '/style.css',
-    '/favicon.ico',
-    '/icons/icon-32.png',
-    '/icons/icon-64.png',
-    '/icons/icon-96.png',
-    '/icons/icon-128.png',
-    '/icons/icon-168.png',
-    '/icons/icon-192.png',
-    '/icons/icon-256.png',
-    '/icons/icon-512.png',
-    '/icons/maskable_icon.png',
-];
 
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
