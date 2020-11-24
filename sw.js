87847191b85f391f57d71c4e053b0676e5ab7ce2
Vincent <tourneretv5@gmail.com>
2020-11-24 10:57:12 +0100

    test serviceWorker change

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
 });
 
-var cacheName = 'js13kPWA-v1';
+var cacheName = 'ginko-v1';
 
 var contentToCache = [
     '/',
@@ -32,5 +32,16 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-    console.log('[Service Worker] Fetched resource '+e.request.url);
+    e.respondWith(
+        caches.match(e.request).then((r) => {
+            console.log('[Service Worker] Fetching resource: '+e.request.url);
+            return r || fetch(e.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+                    console.log('[Service Worker] Caching new resource: '+e.request.url);
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
 });
