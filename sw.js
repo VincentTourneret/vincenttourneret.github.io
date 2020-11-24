cea60eef8d4aca59bb17ba0a51a894695050ad89
Vincent <tourneretv5@gmail.com>
2020-11-24 11:19:02 +0100

    test serviceWorker change

--- sw.js
+++ sw.js
@@ -4,20 +4,20 @@ self.addEventListener('install', (e) => {
     var cacheName = 'ginko-v1';
 
     var contentToCache = [
-        '/index.html',
-        '/app.js',
-        '/style.css',
-        '/favicon.ico',
-        '/icons/icon-32.png',
-        '/icons/icon-64.png',
-        '/icons/icon-96.png',
-        '/icons/icon-128.png',
-        '/icons/icon-168.png',
-        '/icons/icon-180.png',
-        '/icons/icon-192.png',
-        '/icons/icon-256.png',
-        '/icons/icon-512.png',
-        '/icons/maskable_icon.png'
+        'index.html',
+        'app.js',
+        'style.css',
+        'favicon.ico',
+        'icons/icon-32.png',
+        'icons/icon-64.png',
+        'icons/icon-96.png',
+        'icons/icon-128.png',
+        'icons/icon-168.png',
+        'icons/icon-180.png',
+        'icons/icon-192.png',
+        'icons/icon-256.png',
+        'icons/icon-512.png',
+        'icons/maskable_icon.png'
     ];
 
     e.waitUntil(
